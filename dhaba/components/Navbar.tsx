8345a71fa27926004ae622f63ab5c14eb319cc94
naushadraien <naushadraien5@gmail.com>
2023-09-12 16:54:33 +0545

    added mobile menu

--- dhaba/components/Navbar.tsx
+++ dhaba/components/Navbar.tsx
@@ -1,16 +1,39 @@
 import MenuPage from "@/app/menu/page";
 import Link from "next/link";
 import Sidebar from "./Sidebar";
+import CartIcon from "./CartIcon";
 
 const Navbar = () => {
+  const links = [
+    { id: 1, title: "Home", path: "/" },
+    { id: 2, title: "Menu", path: "/menu" },
+    { id: 3, title: "Working Hours", path: "/" },
+    { id: 4, title: "Contact Us", path: "/" },
+  ];
+
+  const user = false;
+
   return (
     <div className="h-12 text-red-500 flex justify-between items-center p-4 border-b-2 border-b-red-500">
       {/* Logo */}
       <div className="text-xl">
         <Link href="/">BabaKaDhaba</Link>
       </div>
+      <div className="hidden md:flex gap-4">
+        {links.map((link) => (
+          <Link href={link.path} key={link.id}>
+            {link.title}
+          </Link>
+        ))}
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon />{" "}
+      </div>
       {/* MobileMenu */}
-      <div>
+      <div className="md:hidden">
         <Sidebar />
       </div>
     </div>
--- dhaba/components/Sidebar.tsx
+++ dhaba/components/Sidebar.tsx
@@ -1,9 +1,58 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import CartIcon from "./CartIcon";
 
 const Sidebar = () => {
-  return <div>
-    <Image src="/open.png" alt="open-menu" width={20} height={20} />
-  </div>;
+  const links = [
+    { id: 1, title: "Home", path: "/" },
+    { id: 2, title: "Menu", path: "/menu" },
+    { id: 3, title: "Working Hours", path: "/" },
+    { id: 4, title: "Contact Us", path: "/" },
+  ];
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  //Temporary User
+  const user = false;
+  return (
+    <div onClick={handleOpen}>
+      {!isOpen ? (
+        <Image src="/open.png" alt="open-menu" width={20} height={20} />
+      ) : (
+        <Image src="/close.png" alt="open-menu" width={20} height={20} />
+      )}
+      {/* <div>
+        {isOpen && ( // if isOpen is true then render the div
+          <div className="absolute top-0 left-0 w-full h-screen bg-white z-10">
+            {links.map((link) => (
+              <div key={link.id} className="text-center text-2xl py-4">
+                <Link href={link.path}>{link.title}</Link>
+              </div>
+            ))}
+          </div>
+        )}
+      </div> */}
+      {isOpen && (
+        <div className="bg-red-500 text-white absolute left-0 top-24 h-[calc(100vh-6rem)] flex justify-center items-center flex-col text-3xl gap-7 w-full z-10">
+          {links.map((link) => (
+            <Link href={link.path} key={link.id}>
+              {link.title}
+            </Link>
+          ))}
+          {!user ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <Link href="/orders">Orders</Link>
+          )}
+          <CartIcon />{" "}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
