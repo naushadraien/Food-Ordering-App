613134282cc07adddc5b3c16d27ea84e45665657
naushadraien <naushadraien5@gmail.com>
2023-09-13 18:17:46 +0545

    working on featured product section

--- dhaba/components/FeaturedItems.tsx
+++ dhaba/components/FeaturedItems.tsx
@@ -1,5 +1,41 @@
+import Image from "next/image";
+import { featuredProducts } from "@/data";
+
 const FeaturedItems = () => {
-  return <div>FeaturedItems</div>;
+  return (
+    <div className="w-screen overflow-x-scroll text-red-500 hover:bg-fuchsia-50 transition-all duration-300">
+      {/* Wrapper */}
+      <div className="w-max flex">
+        {/* Single Item */}
+        {featuredProducts.map((product) => (
+          <div key={product.id}>
+            <div className="w-screen h-[60vh] flex flex-col items-center justify-around p-4">
+              {/* Image Container */}
+              {product.img && (
+                <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
+                  <Image
+                    src={product.img}
+                    alt={product.title}
+                    className="object-contain"
+                    fill
+                  />
+                </div>
+              )}
+              {/* Text Container */}
+              <div className="flex-1 flex flex-col gap-4 items-center text-center">
+                <h1 className="text-xl font-bold uppercase">{product.title}</h1>
+                <p className="p-4">{product.desc}</p>
+                <span className="text-xl font-bold">Rs. {product.price}</span>
+                <button className="bg-red-500 text-white p-2 rounded-md">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedItems;
--- dhaba/components/Slider.tsx
+++ dhaba/components/Slider.tsx
@@ -23,24 +23,24 @@ const Slider = () => {
     },
   ];
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     // setCurrentSlider((prev) => {
-  //     //   if (prev === data.length - 1) {
-  //     //     // here if prev is 2 then it will return 0 and here 2 is the maximum index of the data array
-  //     //     return 0;
-  //     //   }
-  //     //   return prev + 1;
-  //     // });
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // setCurrentSlider((prev) => {
+      //   if (prev === data.length - 1) {
+      //     // here if prev is 2 then it will return 0 and here 2 is the maximum index of the data array
+      //     return 0;
+      //   }
+      //   return prev + 1;
+      // });
 
-  //     //another method for slider for minimize the code
-  //     setCurrentSlider((prev) => (prev === data.length - 1 ? 0 : prev + 1));
-  //   }, 2000);
-  //   return () => clearInterval(interval); //this is for clear the interval
-  // }, [data.length]);
+      //another method for slider for minimize the code
+      setCurrentSlider((prev) => (prev === data.length - 1 ? 0 : prev + 1));
+    }, 2000);
+    return () => clearInterval(interval); //this is for clear the interval
+  }, [data.length]);
 
   return (
-    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row">
+    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
       <div className="flex flex-1 justify-center items-center flex-col gap-8 font-bold text-red-500">
         <h1 className="text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl">
           {data[currentSlider].title}
@@ -49,11 +49,11 @@ const Slider = () => {
           Order Now!
         </button>
       </div>
-      <div className="w-full flex-1 relative ">
+      <div className="w-full flex-1 relative">
         <Image
           src={data[currentSlider].image}
-          alt="silde-1"
-          className="object-cover "
+          alt="sildes"
+          className="object-cover"
           fill
         />
       </div>
