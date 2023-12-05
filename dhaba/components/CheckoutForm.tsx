0f38d59df79099a2c7e2eb49b7696752abb58b31
naushadraien <naushadraien5@gmail.com>
2023-12-05 17:37:24 +0545

    refactor: code refactoring

--- dhaba/components/CheckoutForm.tsx
+++ dhaba/components/CheckoutForm.tsx
@@ -61,7 +61,7 @@ export const CheckoutForm = ({ Orderid }: { Orderid: String }) => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `http://localhost:3000/${Orderid}`,
+        return_url: `https://food-ordering-app-umber.vercel.app/${Orderid}`,
       },
     });
 
